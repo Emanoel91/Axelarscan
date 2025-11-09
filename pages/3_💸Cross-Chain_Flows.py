c32dd7e0d861b15e33925d7d2b7e32c0cbdbe555
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-11-09 17:46:19 +0330

    Update 3_💸Cross-Chain_Flows.py

--- pages/3_💸Cross-Chain_Flows.py
+++ pages/3_💸Cross-Chain_Flows.py
@@ -185,7 +185,7 @@ if run_button:
 
             # Normalize bubble sizes
             max_vol = df_comb_sorted["abs_volume"].max()
-            df_comb_sorted["bubble_size"] = df_comb_sorted["abs_volume"].apply(lambda v: 25 + (v / max_vol) * 80)
+            df_comb_sorted["bubble_size"] = df_comb_sorted["abs_volume"].apply(lambda v: 35 + (v / max_vol) * 80)
 
             # Random positions closer together for tighter packing
             def generate_non_overlapping_positions(sizes, width=1.0, height=1.0, padding=0.01, max_iter=5000):
@@ -201,7 +201,6 @@ if run_button:
                             positions.append((x, y, radius))
                             break
                     else:
-                        # اگر نتوانستیم جای مناسب پیدا کنیم، قبول کنیم با کمی فاصله کم‌تر
                         positions.append((x, y, radius))
                 xs, ys = zip(*[(p[0], p[1]) for p in positions])
                 return np.array(xs), np.array(ys)
