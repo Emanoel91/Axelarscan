90680de00222fbb3158359eb88224dc6cb83605b
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2026-02-10 02:26:47 +0330

    Update 1_🧾AXL_Token_Info.py

--- pages/1_🧾AXL_Token_Info.py
+++ pages/1_🧾AXL_Token_Info.py
@@ -18,7 +18,6 @@ st.set_page_config(
 # --- Title -----------------------------------------------------------------------------------------------------
 st.title("📊 AXL Token Info")
 
-st.info("📊Charts initially display data for a default time range. Select a custom range to view results for your desired period.")
 st.info("⏳On-chain data retrieval may take a few moments. Please wait while the results load.")
 
 # --- Sidebar Footer Slightly Left-Aligned ---------------------------------------------------------------------
